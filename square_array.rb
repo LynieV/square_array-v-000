475391e7739db2ed3217393df4733a55ce952499
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-23 17:50:05 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(numbers)
   # your code here
   
-  numbers.each do |number|
+  numbers.each ** 2 |number|
     
   # store and square
   # number.square!
@@ -12,5 +12,5 @@ def square_array(numbers)
   # number ** number
   # #{number} ** #{number}
   
-  end
+  
 end
